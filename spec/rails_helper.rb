6cf3e6ce2669b31c030026de8d0617de20368602
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-10-16 19:40:13 -0600

    Update rails_helper with shoulda matchers and factory bot

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -44,6 +44,8 @@ RSpec.configure do |config|
   # instead of true.
   config.use_transactional_fixtures = true
 
+  # config.include FactoryBot::Syntax::Methods
+
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
   # `post` in specs under `spec/controllers`.
@@ -63,4 +65,11 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+end
+
+Shoulda::Matchers.configure do |config|
+	config.integrate do |with|
+		with.test_framework :rspec
+		with.library :rails
+	end
 end
